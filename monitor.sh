e19ed0ae536d4b27232ec93c7cf33289c108c399
Maeght Loan <37497007+loan-mgt@users.noreply.github.com>
2024-05-17 20:19:59 +0000

    add wating anim

--- monitor.sh
+++ monitor.sh
@@ -1,57 +1,25 @@
 #!/bin/bash
- 
- 
- 
-# Check if a file argument is provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <file_to_monitor>"
+
+# Check if the correct number of arguments are passed
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <directory_to_monitor> <file_extension> <command_to_execute>"
     exit 1
 fi
- 
- 
- 
-# File to monitor passed as argument
-FILE="$1"
- 
- 
- 
-# Check if the file exists
-if [ ! -f "$FILE" ]; then
-    echo "Error: File '$FILE' not found."
-    exit 1
-fi
- 
- 
- 
-# Get the initial modification time of the file
-LTIME=$(stat -c %Z "$FILE")
- 
- 
- 
-# Function to execute the command when file is modified
-execute_command() {
-    go run main.go
-    # Add your command to run here
-}
- 
- 
- 
-while true; do
-    # Get the current modification time of the file
-    ATIME=$(stat -c %Z "$FILE")
- 
- 
- 
-    # Check if the modification time has changed
-    if [[ "$ATIME" != "$LTIME" ]]; then
-        execute_command
-        # Update the last modification time
-        LTIME=$ATIME
-    fi
- 
- 
- 
-    # Sleep for a short duration before checking again
-    sleep 5
+
+# Directory to monitor
+DIR_TO_MONITOR="$1"
+
+# File extension to monitor
+FILE_EXTENSION="$2"
+
+# Command to execute on change
+COMMAND_TO_EXECUTE="$3"
+
+# Use inotifywait to monitor the directory for create, modify, and delete events
+inotifywait -m -e create -e modify -e delete --format '%w%f' "$DIR_TO_MONITOR" | while read FILE
+do
+
+        $COMMAND_TO_EXECUTE
+        echo "Change detected in file: $FILE"
+
 done
- 
