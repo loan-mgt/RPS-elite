3f8be057d594687587dee139c27f7348beb7145a
Maeght Loan <37497007+loan-mgt@users.noreply.github.com>
2024-05-24 21:54:50 +0000

    fix

--- internal/handlers/moveHandler.go
+++ internal/handlers/moveHandler.go
@@ -51,87 +51,24 @@ func HandleMove(message []byte, conn *websocket.Conn) error {
 		return err
 	}
 
-	opponent, err := services.GetOpponent(player.Name)
-	if err != nil {
-		log.Println("Unalbe to get oppoent infos:", err)
-	} else {
+	if ready, _ := services.HaveAllPlayersSelectedMove(); ready {
 
-		if opponent.Move != nil {
-
-			opponent, err := services.GetOpponent(player.Name)
+		opponent, err := services.GetOpponent(player.Name)
+		if err != nil {
+			log.Println("Failed getting opponent:", err)
+		} else {
+			err = senders.SendMove(opponent.Conn, "opponent", &request.Move)
 			if err != nil {
-				log.Println("Failed getting opponent:", err)
-			} else {
-				err = senders.SendMove(opponent.Conn, "opponent", &request.Move)
-				if err != nil {
-					log.Println("Failed to send opponent move:", err)
-				}
-
-				err = senders.SendMove(player.Conn, "opponent", opponent.Move)
-				if err != nil {
-					log.Println("Failed to send opponent move:", err)
-				}
-
-				message := "next round in 3s"
-				if err := senders.SendMessage(conn, message); err != nil {
-					log.Println("Error sending message to player:", err)
-				}
-
-				opponent, err := services.GetOpponent(player.Name)
-				if err != nil {
-					log.Println("Failed getting opponent:", err)
-				} else {
-					if err := senders.SendMessage(opponent.Conn, message); err != nil {
-						log.Println("Error sending message to opponent:", err)
-					}
-				}
-
-				go func() {
-					time.Sleep(3 * time.Second)
-
-					services.SetPlayerMove(player.Name, "")
-
-					services.SetPlayerMove(opponent.Name, "")
-
-					err = senders.ResetMove(opponent.Conn, "player")
-					if err != nil {
-						log.Println("Failed to send player move:", err)
-					}
-
-					err = senders.ResetMove(opponent.Conn, "opponent")
-					if err != nil {
-						log.Println("Failed to send opponent move:", err)
-					}
-
-					err = senders.ResetMove(player.Conn, "player")
-					if err != nil {
-						log.Println("Failed to send player move:", err)
-					}
-
-					err = senders.ResetMove(player.Conn, "opponent")
-					if err != nil {
-						log.Println("Failed to send opponent move:", err)
-					}
-
-					err = senders.SetScore(player.Conn, "opponent", 2)
-					if err != nil {
-						log.Println("Failed to send opponent score:", err)
-					}
-
-					err = senders.SetScore(opponent.Conn, "opponent", 2)
-					if err != nil {
-						log.Println("Failed to send opponent score:", err)
-					}
-
-				}()
-
+				log.Println("Failed to send opponent move:", err)
 			}
 
-		} else {
-			messagePlayer := "Waiting for your opponent to make a move"
-			messageOpponent := fmt.Sprintf("%s is waiting on you, please select a move", player.Name)
+			err = senders.SendMove(player.Conn, "opponent", opponent.Move)
+			if err != nil {
+				log.Println("Failed to send opponent move:", err)
+			}
 
-			if err := senders.SendMessage(conn, messagePlayer); err != nil {
+			message := "next round in 3s"
+			if err := senders.SendMessage(conn, message); err != nil {
 				log.Println("Error sending message to player:", err)
 			}
 
@@ -139,11 +76,67 @@ func HandleMove(message []byte, conn *websocket.Conn) error {
 			if err != nil {
 				log.Println("Failed getting opponent:", err)
 			} else {
-				if err := senders.SendMessage(opponent.Conn, messageOpponent); err != nil {
+				if err := senders.SendMessage(opponent.Conn, message); err != nil {
 					log.Println("Error sending message to opponent:", err)
 				}
 			}
 
+			go func() {
+				time.Sleep(3 * time.Second)
+
+				services.SetPlayerMove(player.Name, "")
+
+				services.SetPlayerMove(opponent.Name, "")
+
+				err = senders.ResetMove(opponent.Conn, "player")
+				if err != nil {
+					log.Println("Failed to send player move:", err)
+				}
+
+				err = senders.ResetMove(opponent.Conn, "opponent")
+				if err != nil {
+					log.Println("Failed to send opponent move:", err)
+				}
+
+				err = senders.ResetMove(player.Conn, "player")
+				if err != nil {
+					log.Println("Failed to send player move:", err)
+				}
+
+				err = senders.ResetMove(player.Conn, "opponent")
+				if err != nil {
+					log.Println("Failed to send opponent move:", err)
+				}
+
+				err = senders.SetScore(player.Conn, "opponent", 2)
+				if err != nil {
+					log.Println("Failed to send opponent score:", err)
+				}
+
+				err = senders.SetScore(opponent.Conn, "opponent", 2)
+				if err != nil {
+					log.Println("Failed to send opponent score:", err)
+				}
+
+			}()
+
+		}
+
+	} else {
+		messagePlayer := "Waiting for your opponent to make a move"
+		messageOpponent := fmt.Sprintf("%s is waiting on you, please select a move", player.Name)
+
+		if err := senders.SendMessage(conn, messagePlayer); err != nil {
+			log.Println("Error sending message to player:", err)
+		}
+
+		opponent, err := services.GetOpponent(player.Name)
+		if err != nil {
+			log.Println("Failed getting opponent:", err)
+		} else {
+			if err := senders.SendMessage(opponent.Conn, messageOpponent); err != nil {
+				log.Println("Error sending message to opponent:", err)
+			}
 		}
 
 	}
