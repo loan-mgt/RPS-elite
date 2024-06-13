bdf3a6798a3a2529a25c5554c7c3311678f670f1
Maeght Loan <37497007+loan-mgt@users.noreply.github.com>
2024-06-13 20:17:42 +0000

    init login request

--- internal/controllers/indexController.go
+++ internal/controllers/indexController.go
@@ -4,25 +4,38 @@ import (
 	"log"
 	"net/http"
 
+	templatedata "rcp/elite/internal/types/template-data"
 	"rcp/elite/internal/utils"
 )
 
 func IndexController(w http.ResponseWriter, r *http.Request) {
-	// Check if the 'stk' cookie is set
+
+	data := templatedata.IndexData{
+		Main: "logged",
+	}
 	cookie, err := r.Cookie("stk")
 	if err != nil {
-		err = utils.Templates.ExecuteTemplate(w, "index", nil)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+		data.Main = "home"
+
+		defaultCookie := http.Cookie{
+			Name:     "stk",
+			Value:    "default_value_here",
+			MaxAge:   60 * 60 * 24 * 60, // 2 months in seconds (60 seconds * 60 minutes * 24 hours * 60 days)
+			HttpOnly: true,
+			Secure:   true,
+			SameSite: http.SameSiteStrictMode,
 		}
+		http.SetCookie(w, &defaultCookie)
+		log.Println("Cookie 'stk' set with default value")
+
+	} else {
+		// If the cookie is found, you can access its value with cookie.Value
+		log.Println("Cookie 'stk' found with value:", cookie.Value)
 	}
 
-	// If the cookie is found, you can access its value with cookie.Value
-	log.Println("Cookie 'stk' found with value:", cookie.Value)
-
-	err = utils.Templates.ExecuteTemplate(w, "logged", nil)
+	err = utils.Templates.ExecuteTemplate(w, "index", data)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-	
+
 }
---
+++ internal/types/template-data/index.go
@@ -0,0 +1,5 @@
+package templatedata
+
+type IndexData struct {
+	Main string
+}
